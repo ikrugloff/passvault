0cc73bdcd54e520ef07ec5597ae85fa3e0fc1ef9
Foxfanmedium <32788485+Foxfanmedium@users.noreply.github.com>
2018-09-06 22:09:02 +0300

    Доработка окна регистрации + проверка/генрация матер пароля

--- gui/registration_page.py
+++ gui/registration_page.py
@@ -1,15 +1,36 @@
 #Графический интерфейс пользователского окна регистрации
 import sys
-from PyQt5 import QtWidgets
-from PyQt5 import uic
+import time
+
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from registration_page_ui import *
 
 
-app = QtWidgets.QApplication(sys.argv)
-window = uic.loadUi('registration_page.ui')
-print(window.pushButton)
-window.show()
+class MyWin(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+        self.ui = Ui_MyWin()
+        self.ui.setupUi(self)
+        # self.ui.pushButtonSave.clicked.connect(self.stop)
+        # self.ui.pushButtonClose.clicked.connect(self.close)  #   Удалить при рефакторинге
+        # self.ui.actionExit.triggered.connect(self.close)  #   Удалить при рефакторинге
 
-def my_f():
-    pass
+    # def stop(self):        # убрать эту функцию. Избыточно. Удалить при рефакторинге
+        # self.terminate()
 
-sys.exit(app.exec_())
+    def closeEvent(self, e):    # Функция открытия диалогового окна для подтверждения закрытия окна регистрации
+        result = QtWidgets.QMessageBox.question(self, "ConfirmDIalog", "Really quit?",
+                                                QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No)
+        if result == QtWidgets.QMessageBox.Yes:
+            e.accept()
+        else:
+            e.ignore()
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    myapp = MyWin()
+    # myapp = uic.loadUi('registration_page.ui')
+    # print(myapp.pushButtonClose)
+    myapp.show()
+    sys.exit(app.exec_())
