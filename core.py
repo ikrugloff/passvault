8d8adeb4920a98369794a73aa811b346cd2d5368
Ilia Kruglov <i9200418888@gmail.com>
2018-09-17 23:50:25 +0300

    Sprint delta test were implemented.

--- core.py
+++ core.py
@@ -1,40 +1,46 @@
 import sys
 import crypt_db
 
-password_check = crypt_db.check_master_password()
 
-while password_check is None:
-        new_master_password = str(input('My lord, create a new master password please! >'))
-        new_master_password = crypt_db.encrypt(new_master_password).decode('utf-8')
-        crypt_db.create_master_password(new_master_password)
-        password_check = crypt_db.check_master_password()
+def main():
+    password_check = crypt_db.check_master_password()
 
-print('<--Greetings you!-->')
-print()
+    while password_check is None:
+            new_master_password = str(input('My lord, create a new master password please! >'))
+            new_master_password = crypt_db.encrypt(new_master_password).decode('utf-8')
+            crypt_db.create_master_password(new_master_password)
+            password_check = crypt_db.check_master_password()
 
-USERNAME = crypt_db.get_master_login()
-PASSWORD = crypt_db.get_master_pass()
+    print('<--Greetings you!-->')
+    # print()
 
-usernameGuess = input('What is your name stranger? > ')
-if usernameGuess == 'q' or usernameGuess == 'Q':
-    sys.exit()
+    USERNAME = crypt_db.get_master_login()
+    PASSWORD = crypt_db.get_master_pass()
 
-passwordGuess = input('I need a key to save your secrets... > ')
-if passwordGuess == 'q' or usernameGuess == 'Q':
-    sys.exit()
-
-while True:
-
-    if USERNAME == usernameGuess and PASSWORD == passwordGuess:
-        print(f'\n<--Welcome back master {usernameGuess}!-->\n')
-        crypt_db.output()
-    else:
-        print('\nYou are not prepared!!!')
-
-    print('\n<--Greetings you!-->')
     usernameGuess = input('What is your name stranger? > ')
     if usernameGuess == 'q' or usernameGuess == 'Q':
-        break
+        sys.exit()
+
     passwordGuess = input('I need a key to save your secrets... > ')
-    if passwordGuess == 'q' or usernameGuess == 'Q':
-        break
+    if passwordGuess == 'q' or passwordGuess == 'Q':
+        sys.exit()
+
+    while True:
+
+        if USERNAME == usernameGuess and PASSWORD == passwordGuess:
+            print(f'\n<--Welcome back master {usernameGuess}!-->\n')
+            crypt_db.output()
+            break
+        else:
+            print('\nYou are not prepared!!!')
+
+        print('\n<--Greetings you!-->')
+        usernameGuess = input('What is your name stranger? > ')
+        if usernameGuess == 'q' or usernameGuess == 'Q':
+            break
+        passwordGuess = input('I need a key to save your secrets... > ')
+        if passwordGuess == 'q' or passwordGuess == 'Q':
+            break
+
+if __name__ == '__main__':
+    main()
