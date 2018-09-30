6fe443a6eda6ef26d57c4f9294bc5182780a4111
ikrugloff <i9200418888@gmail.com>
2018-09-30 17:50:10 +0300

    A function run() were added.

--- start_script.py
+++ start_script.py
@@ -3,7 +3,14 @@ import crypt_db
 
 password_check = crypt_db.check_master_password()
 
-if password_check is None:
-    os.system('python registration_page.py')
-else:
-    os.system('python login_page.py')
+
+def run():
+    if password_check is None:
+        os.system('python registration_page.py')
+    else:
+        os.system('python login_page.py')
+
+
+if __name__ == '__main__':
+    run()
+
